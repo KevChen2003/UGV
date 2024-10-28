f31d899309881917cf6627a744f648b20f7056ba
DO-NOT-TOUCH <z5360742@ad.unsw.edu.au>
2024-10-28 20:57:30 +1100

    TMM thread function done

--- MTRN3500 Assignment 2/TMM.cpp
+++ MTRN3500 Assignment 2/TMM.cpp
@@ -37,6 +37,45 @@ void ThreadManagement::threadFunction() {
 			gcnew ThreadProperties(gcnew ThreadStart(gcnew Display(), &Display::threadFunction), true, bit_DISPLAY, "Display Thread"),
 			gcnew ThreadProperties(gcnew ThreadStart(gcnew CrashAvoidance(), &CrashAvoidance::threadFunction), true, bit_CRASHAVOIDANCE, "Crash Avoidance Thread")
 	};
+	// list of threads
+	ThreadList = gcnew array<Thread^>(ThreadPropertiesList->Length);
+
+	// list of stopwatches
+	StopwatchList = gcnew array<Stopwatch^>(ThreadPropertiesList->Length);
+
+	// make thread barrier, number of threads = all threads in list + TMM thread
+	SM_TM_->ThreadBarrier = gcnew Barrier(ThreadPropertiesList->Length + 1);
+
+	for (int i = 0; i < ThreadPropertiesList->Length; i++) {
+		// initialise all stopwatches
+		StopwatchList[i] = gcnew Stopwatch;
+		// initialise all the threads before starting
+		ThreadList[i] = gcnew Thread(ThreadPropertiesList[i]->ThreadStart_);
+		// start all threads
+		ThreadList[i]->Start();
+	}
+
+	// wait at TMT thread barrier
+	SM_TM_->ThreadBarrier->SignalAndWait();
+
+	// start all stopwatches
+	for (int i = 0; i < ThreadList->Length; i++) {
+		StopwatchList[i]->Start();
+	}
+	// start thread loop
+	// !ConsoleKeyAvailable means that it will stop running once a key has been pressed
+	while (!Console::KeyAvailable && getShutdownFlag()) {
+		Console::WriteLine("TMT Thread is running");
+		processHeartBeats();
+		Thread::Sleep(50);
+	}
+
+	// shutdown threads
+	shutdownModules();
+	// join all threads
+	for (int i = 0; i < ThreadPropertiesList->Length; i++) {
+		ThreadList[i]->Join();
+	}
 }
 
 error_state ThreadManagement::processHeartBeats() {
